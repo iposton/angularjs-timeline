47ff82b2c5b5c45e30f9970df9c1c3a7ffe8fb28
iposton <ianposton@sbcglobal.net>
2017-06-20 17:02:04 -0700

    improve timeline with 3 away players

--- app/app.js
+++ app/app.js
@@ -29,8 +29,9 @@
                                     <div>
                                         <md-input-container>
                                             <label>Minutes...</label>
-                                            <input type="number" id="minutes" ng-model="vm.mins" ng-change="vm.addAction(vm.playerMinutes, vm.team)" min="0" max="12" />
+                                            <input type="number" id="minutes" ng-model="vm.mins" ng-change="vm.addAction(vm.playerMinutes, vm.team)" min="0" max="12" />{{vm.playerMinutes}}
                                         </md-input-container>
+                                        
                                         {{vm.move}}
                                     </div>
                                 </div>
@@ -40,6 +41,7 @@
                                     <div ng-repeat="h in vm.team.home.players" style="display:inline-block">
                                         <div id='{{h.id}}' class="mark" ng-class="{sameminuteshome: vm.sameMinutesHome.length > 1}">{{h.initials}}</div>
                                     </div>
+                                    
                                     <div ng-if="vm.sameMinutesHome.length > 1" class="sameminutes-home-block" style="left:{{vm.sameMinutesHome[0].counter}}px">{{vm.sameMinutesHome.length}}</div>
                                     <ol>
                                         <li><span>72</span></li>
@@ -53,10 +55,23 @@
                                         <li><span>648</span></li>
                                         <li><span>720</span></li>
                                     </ol>
-                                    <div ng-repeat="a in vm.team.away.players" style="display:inline-block">
-                                        <div id='{{a.id}}' class="mark red" ng-class="{sameminutesaway: vm.sameMinutesAway.length > 1}">{{a.initials}}</div>
+                             
+                                    <div ng-repeat="a in vm.team.away.players | limitTo: 1" style="display:inline-block">
+                                    
+                
+                                   
+                                        <div ng-if="vm.countZero > 1" class="sameminutes-away-block" style="">{{vm.countZero}}a</div>
+                                        <div ng-if="vm.countSame > 1" class="sameminutes-away-block" style="left:{{vm.holdCounter}}px">{{vm.countSame}}b</div>
                                     </div>
-                                    <div ng-if="vm.sameMinutesAway.length > 1" class="sameminutes-away-block" style="left:{{vm.sameMinutesAway[0].counter}}px">{{vm.sameMinutesAway.length}}</div>
+                                    <div ng-repeat="a in vm.team.away.players" style="display:inline-block">
+                                    
+                
+                                        <div id='{{a.id}}' class="mark red" ng-class="{sameminutesaway: a.same === true}" style="left:{{a.counter}}px">{{a.initials}}</div>
+                                     
+                                    </div>
+
+                                    
+                     
                             </section>
                             <div class="instructions">
                                 Home = <span class="home-block">&nbsp&nbsp </span> &nbsp Away = <span class="away-block">&nbsp&nbsp </span> </br> Select a player in the side navigation and then use the input box to add Minutes played. The Max value is 12 minutes. This Timeline represents one quarter of professional basketball with a timeline displaying 12 minutes in seconds.
@@ -75,7 +90,6 @@
                 link: function($scope, iElm) {
                     if ($scope.index === 0) {
                         $timeout(function() {
-
                             iElm.triggerHandler('click');
                             $mdSidenav('left').toggle();
                         }, 0);
@@ -96,13 +110,17 @@
         self.setActive = setActive;
         self.toggleList = toggleList;
 
+
         //Globals
         self.initial = '';
         self.mins = 0;
         self.sameMinutesHome = [];
-        self.sameMinutesAway = [];
+
         var selectedPlayer = '';
         var selectedId = '';
+
+
+
 
         //Define team data object
         //Iterate in view to display values
@@ -113,19 +131,27 @@
                         player_name: 'LeBron James',
                         initials: 'LJ',
                         counter: 0,
-                        location: 'away'
+                        location: 'away',
+                        same: false,
+                        last: false
+
                     }, {
                         id: 'h2',
                         player_name: 'Kyrie Irving',
                         initials: 'KI',
                         counter: 0,
-                        location: 'away'
+                        location: 'away',
+                        same: false,
+                        last: false
+
                     }, {
                         id: 'h3',
                         player_name: 'Richard Jefferson',
                         initials: 'RJ',
                         counter: 0,
-                        location: 'away'
+                        location: 'away',
+                        same: false,
+                        last: false
                     }
 
                 ]
@@ -136,13 +162,17 @@
                         player_name: 'Kevin Durant',
                         initials: 'KD',
                         counter: 0,
-                        location: 'home'
+                        location: 'home',
+                        same: false,
+                        last: false
                     }, {
                         id: 'a2',
                         player_name: 'Steph Curry',
                         initials: 'SC',
                         counter: 0,
-                        location: 'home'
+                        location: 'home',
+                        same: false,
+                        last: false
                     }
 
                 ]
@@ -159,6 +189,7 @@
         }
 
         function addAction(timeInSeconds, team) {
+            //self.addMore = addMore;
 
             //Define Home and Away players from team object
             var home = team.home.players;
@@ -171,10 +202,12 @@
             self.playerMinutes = selectedPlayer.counter;
             //Style div by selected id move left time represented in pixels
             d.style.left = minutes + 'px';
+            var sameToItself;
 
-            if (selectedPlayer.location === 'home') {
+
+            if (self.sameMinutesHome.length <= 2 && selectedPlayer.location === 'home') {
                 home.filter(function(h) {
-                    if (h.counter === minutes) {
+                    if (self.sameMinutesHome.length < 2 && h.counter === minutes) {
                         h.same = true;
                         self.sameMinutesHome.push(h);
 
@@ -195,20 +228,118 @@
             }
 
             if (selectedPlayer.location === 'away') {
-                away.filter(function(a) {
-                    if (a.counter === minutes) {
+                var countZero = 0;
+                var countSame = 0;
+
+                angular.forEach(away, function(a) {
+
+                    if (a.counter === 0) {
                         a.same = true;
-                        self.sameMinutesAway.push(a);
+                        //count counters that are at zero minutes
+                        countZero += 1;
+                        self.countZero = countZero;
+                        console.log(self.countZero, 'count for zero counter');
+
+                        //if a user goes back to zero
+                        if (a.counter === 0 && countZero > 1) {
+                            a.same = true;
+                        }
+
+                        // if (countSame === 1) {
+                        //     console.log(countSame, "countSame at zero");
+                        //     //a.same = false;
+                        //     self.countSame = countSame;
+                        //     countSame = countSame;
+                        //     console.log(countSame, 'countSame at zero dont count');
+                        // } 
+
+                        //sameToItself = false;
+
                     }
 
 
-                });
 
-                away.filter(function(a) {
-                    if (a.counter != minutes) {
+                    if (a.counter > 0) {
+
+
+
+                        if (a.counter === selectedPlayer.counter) {
+
+                            countSame += 1;
+                            self.countSame = countSame;
+
+                            sameToItself = true;
+                            //console.log(a, 'nothing mathces my counter Iam false');
+                            a.same = false;
+                            //sameToItself = countSame;
+                            console.log(countSame, 'if nothing matches selected should equal 1 else 2');
+                            console.log(a, 'this player matches selectedPlayer for same counters');
+                            self.holdCounter = a.counter;
+                            //if same count is 1 do something keep piece false
+                            //if count is 2 make a group make piece true to hide and only show group 
+
+                        }
+
+                    }
+
+                })
+
+                var countNotSelected = 0;
+                angular.forEach(away, function(a) {
+
+                    if (a.counter > 0) {
+
+                        if (countSame > 1) {
+                            sameToItself = false;
+                            a.same = true;
+                            //countSame += 1;
+                            self.countSame = countSame;
+                            self.holdCounter = a.counter;
+                            console.log(countSame, 'whats the count? should be 2');
+                            console.log(self.holdCounter, 'whats the holdCounter?');
+                            console.log(a, 'we are equal to selected player');
+
+
+                        } else {
+
+
+
+                            if (a.active === false) {
+                                //self.holdCounter = a.counter;
+                                console.log(a, 'countNotSelected');
+                                console.log(self.holdCounter, 'whats the hold counter? if active false');
+                                console.log(a.counter, 'if a.counter === a.counter');
+                                if (a.counter === a.counter) {
+                                    countNotSelected += 1;
+
+                                    self.holdCounter = a.counter;
+                                    console.log(countNotSelected, 'countNotSelected has a match if count is 2 else its solo');
+                                }
+
+                            }
+
+
+
+                        }
+
+                        if (countNotSelected > 1) {
+                            self.countSame = countNotSelected;
+                        }
+
+                        if (countSame > 1 && self.holdCounter != a.counter) {
+                            a.same = false;
+
+                        }
+
+                    }
+
+                    //adjust this if you add more players to team
+                    if (a.counter === 0 && countZero === 1) {
+                        console.log(a, "making this false");
+                        console.log(countSame, "checking countSame when last zero selected");
                         a.same = false;
-                        self.sameMinutesAway = [];
                     }
+
 
                 });
 
@@ -217,11 +348,12 @@
         }
 
         function selectPlayer(player) {
-            console.log(player, 'selected player')
+            console.log(player, 'selected player');
             selectedId = player.id;
             selectedPlayer = player;
             self.mins = player.counter / 60;
             $mdSidenav('left').toggle();
+            self.addAction(self.playerMinutes, self.team);
 
         }
         //Set active class in side-nav
@@ -233,16 +365,12 @@
                 if (h.active) {
                     h.active = false;
                 }
-
-
             })
 
             away.some(function(a) {
                 if (a.active) {
                     a.active = false;
                 }
-
-
             })
 
             self.move = '';
